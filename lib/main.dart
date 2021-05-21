66cac222ce098428d2a48e920bf71bc490ba9e90
Ankit Anand <ankitmonu321@gmail.com>
2021-05-21 15:11:07 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/screens/home_page.dart';
 import 'package:flutter_catalog/screens/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -28,8 +29,10 @@ class MyApp extends StatelessWidget {
       initialRoute: "/", 
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => MyHomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.loginRoute : (context) =>LoginPage(),
+        MyRoutes.homeRoute : (context) =>MyHomePage(),
+        // "/home": (context) => MyHomePage(),
+        // "/login": (context) => LoginPage(),
       },
     );
   }
